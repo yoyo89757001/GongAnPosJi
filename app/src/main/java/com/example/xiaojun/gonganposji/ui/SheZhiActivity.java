af1bfa2b8c7dd1e75498e8d51b9baa2b657e0193
yoyo89757001 <332663557@qq.com>
2017-11-09 14:00:15 +0800

    lllllllllll

--- app/src/main/java/com/example/xiaojun/gonganposji/ui/SheZhiActivity.java
+++ app/src/main/java/com/example/xiaojun/gonganposji/ui/SheZhiActivity.java
@@ -6,7 +6,6 @@ import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
